d9a6c6aec8ca8a5c6b43ed458474b9279bdaad13
SemyonIvanov <semenivanov24@gmail.com>
2021-06-17 12:11:43 +0300

    state

--- src/General/News/News.js
+++ src/General/News/News.js
@@ -1,19 +1,36 @@
-import Swiper_news from "./News_content/Swiper_news";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper.scss';
+import 'swiper/swiper-bundle.css';
 
 import data from './News_data.json'
+
+import SwiperCore, {
+  Pagination,Navigation,EffectCube
+} from 'swiper/core';
+SwiperCore.use([Pagination,Navigation,EffectCube]);
 
 function News() {
   return (
     <Swiper
     spaceBetween={50}
     slidesPerView={1}
+    navigation={true}
+    pagination={{
+      "clickable": true
+    }}
+    loop={true}
+    effect={'cube'} grabCursor={true} cubeEffect={{
+      "shadow": true,
+      "slideShadows": true,
+      "shadowOffset": 20,
+      "shadowScale": 0.94
+    }}
+    className='mySwiper'
   >
     
       {data.map((item) => (
         <SwiperSlide>
-        <div className='news' style={{ background: `center url(${item.image}) no-repeat` }}>
+        <div className='news'>
+          <img src={item.image} />
           <span>{item.textNews}</span>
         </div>
         </SwiperSlide>
